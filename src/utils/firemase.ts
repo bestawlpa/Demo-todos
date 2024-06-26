6a2acde1cd7106e94db77e17b3d85c38779cbdfb
apiwat <bunderscorest@gmail.com>
2024-06-26 17:52:30 +0700

    edit

--- src/utils/firemase.ts
+++ src/utils/firemase.ts
@@ -1,16 +1,20 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCMfW-vzJiOaRsTfXWk7k4W8YRhITPsz1E",
-  authDomain: "todos-66723.firebaseapp.com",
-  projectId: "todos-66723",
-  storageBucket: "todos-66723.appspot.com",
-  messagingSenderId: "791705895966",
-  appId: "1:791705895966:web:67357e2bafc54615451a0f"
+  apiKey: "AIzaSyCo988Q0ncrorkWsT7bGpsSznkrrvh2b_I",
+  authDomain: "test-11933.firebaseapp.com",
+  projectId: "test-11933",
+  storageBucket: "test-11933.appspot.com",
+  messagingSenderId: "1012630499157",
+  appId: "1:1012630499157:web:c76a279fe62d857b0dc9dc",
+  measurementId: "G-ZENVXTPGEZ"
 };
 
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+
+
 
